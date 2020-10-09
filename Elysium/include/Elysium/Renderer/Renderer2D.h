925a904f82c52110b67c4be9890d042050ef10a8
cvu1998 <congvinhvu98@gmail.com>
2020-10-09 00:02:10 -0400

    Updated Elysium Static Library

--- Elysium/include/Elysium/Renderer/Renderer2D.h
+++ Elysium/include/Elysium/Renderer/Renderer2D.h
@@ -12,9 +12,17 @@ namespace Elysium
     class Renderer2D
     {
     private:
-        static void beginBatch();
-        static void endBatch();
-        static void flush();
+        static void beginQuadBatch();
+        static void endQuadBatch();
+        static void flushQuads();
+
+        static void beginLineBatch();
+        static void endLineBatch();
+        static void flushLines();
+
+        static void beginPointBatch();
+        static void endPointBatch();
+        static void flushPoints();
 
     public:
         /***ONLY CALL ONCE***/
@@ -39,9 +47,19 @@ namespace Elysium
         static void drawQuadWithRotation(const glm::vec2& position, const glm::vec2& size, float rotation, const TextureData& texture,
             const glm::vec4& color = { 1.0f, 1.0f, 1.0f, 1.0f });
 
+        static void drawLine(const glm::vec2& p0, const glm::vec2& p1,
+            const glm::vec4& color = { 1.0f, 1.0f, 1.0f, 1.0f });
+
+        static void drawPoint(const glm::vec2& position,
+            const glm::vec4& color = { 1.0f, 1.0f, 1.0f, 1.0f });
+
+        static void setLineWidth(float width);
+        static void setPointSize(float size);
+
         struct Stats {
             unsigned int DrawCount = 0;
             unsigned int QuadCount = 0;
+            unsigned int LineCount = 0;
         };
 
         static Stats& getStats();
